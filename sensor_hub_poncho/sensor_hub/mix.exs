d111462345d557fdc10d8f25f769acf03ea8d034
adam vietro <hunterafi@gmail.com>
2025-08-03 13:12:57 -0700

    Working Firmware Fixed all the firmware issues need to worry about ssh.

--- sensor_hub_poncho/sensor_hub/mix.exs
+++ sensor_hub_poncho/sensor_hub/mix.exs
@@ -71,7 +71,8 @@ defmodule SensorHub.MixProject do
       {:nerves_system_osd32mp1, "~> 0.15", runtime: false, targets: :osd32mp1},
       {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
       {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
-      {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro}
+      {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro},
+      {:vintage_net_wifi, "~> 0.12.0", targets: @all_targets}
     ]
   end
 
