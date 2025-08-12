3c9e9c17da986668f5e369c72ff9ed6581d87fb8
adam vietro <hunterafi@gmail.com>
2025-08-12 14:49:52 -0700

    UV Light File Structure Set up the file strucuture for the UV light sensor. Still need to configure the different addresses and the conversion factors.

---
+++ sensor_hub_poncho/sensor_hub/ltr390_uv/lib/ltr390.ex
@@ -0,0 +1,8 @@
+defmodule LTR390_UV do
+  @moduledoc """
+  Documentation for `Ltr390`.
+  """
+  def hello do
+    :world
+  end
+end
---
+++ sensor_hub_poncho/sensor_hub/ltr390_uv/test/ltr390_test.exs
@@ -0,0 +1,8 @@
+defmodule Ltr390Test do
+  use ExUnit.Case
+  doctest Ltr390
+
+  test "greets the world" do
+    assert Ltr390.hello() == :world
+  end
+end
