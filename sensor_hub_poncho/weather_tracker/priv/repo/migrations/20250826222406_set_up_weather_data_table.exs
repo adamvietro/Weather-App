3d5cd678a6bddc5565b3d23c67a025c8b339d76f
adam vietro <hunterafi@gmail.com>
2025-08-26 15:58:04 -0700

    Migrations Added in the migrations to the github. Running into issues with not being able to use the timescaledb even though it is within the database.

---
+++ sensor_hub_poncho/weather_tracker/priv/repo/migrations/20250826222406_set_up_weather_data_table.exs
@@ -0,0 +1,25 @@
+defmodule WeatherTracker.Repo.Migrations.SetUpWeatherDataTable do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE EXTENSION IF NOT EXISTS timescaledb")
+
+    create table(:weather_conditions, primary_key: false) do
+      add(:timestamp, :naive_datetime, null: false)
+      add(:temperature_c, :decimal, null: false)
+      add(:pressure_pa, :decimal, null: false)
+      add(:humidity_rh, :decimal, null: false)
+      add(:light_lumens, :decimal, null: false)
+      add(:voc_index, :decimal, null: false)
+      add(:uv_index, :decimal, null: false)
+      add(:als_lux, :decimal, null: false)
+    end
+
+    execute("SELECT create_hypertable('weather_conditions', 'timestamp')")
+  end
+
+  def down do
+    drop(table(:weather_conditions))
+    execute("DROP EXTENSION IF EXISTS timescaledb")
+  end
+end
