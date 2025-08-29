c2edfa7f5073806cade5f9433caf57e5f798663d
adam vietro <hunterafi@gmail.com>
2025-08-28 18:07:06 -0700

    weather_conditions_controller.ex Set up the one call to create a new data entry into the db.

---
+++ sensor_hub_poncho/weather_tracker/lib/weather_tracker_web/controllers/weather_conditions_controller.ex
@@ -0,0 +1,29 @@
+defmodule WeatherTracker.WeatherConditions do
+  use WeatherTrackerWeb, :controller
+
+  require Logger
+
+  alias WeatherTracker.{
+    WeatherConditions,
+    WeatherConditions.WeatherCondition
+  }
+
+  def create(conn, params) do
+    IO.inspect(params)
+    case WeatherConditions.create_entry(params) do
+      {:ok, weather_condition = %WeatherCondition{}} ->
+        Logger.debug("Successfully created weather condition: #{inspect(weather_condition)}")
+
+        conn
+        |> put_status(:created)
+        |> json(weather_condition)
+
+      error ->
+        Logger.warning("Failed to create weather condition: #{inspect(error)}")
+
+        conn
+        |> put_status(:unprocessable_entity)
+        |> json(%{message: "Poorly formatted payload"})
+    end
+  end
+end
